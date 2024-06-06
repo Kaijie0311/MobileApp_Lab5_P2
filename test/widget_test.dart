56daec631514c1c07584edbb428534ebec943595
Kai Jie <hogrider0313@gmail.com>
2024-06-06 12:57:03 +0800

    main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lab_five/Activation.dart';
+import 'package:lab_five/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
